d82200bacb8cf8c4c59cda16cc3d7561c3e03e65
Christian4135 <113070331+Christian4135@users.noreply.github.com>
2022-09-28 21:10:12 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+export default function Header() {
+  return (
+    <>
+      <nav class="head">
+        <ul>
+          <li class="head">
+            <a href="/pages/1030">Visual Programming</a>
+          </li>
+          <li class="head">
+            <a href="/pages/1200">Functional Programming</a>
+          </li>
+          <li class="head">
+            <a href="/pages/2350/">Object-Oriented Programming and Design</a>
+          </li>
+          <li class="head">
+            <a href="/pages/2440">Data Structures</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
---
+++ src/pages/2200.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function CalculusforScienceandBusiness() {
+  return (
+    <>
+      <h1>MAT 2200 Calculus for Science and Business</h1>
+      <p>(formerly MAT 220; MAT 120)</p>
+      <p>
+        note that this minor does not suffice for an area of concentration in
+        the Interdisciplinary Studies major (described in the Interdisciplinary
+        Studies section). Powered by SmartCatalog MAT 2200 Calculus for Science
+        and Business close (formerly MAT 220; MAT 120) Limits and continuity;
+        derivatives maxima and minima; antiderivatives; definite integral;
+        fundamental theorem of calculus; techniques of integration. NOTE:
+        Graphing Calculator Required.
+      </p>
+      <h2 id="credit">Credits</h2>3<h2>Prerequisite</h2>
+      Not open to students who have passed MAT 2210.
+      <h2>Distribution</h2>
+      Mathematics/Computer Studies
+      <h2>Offered</h2>
+      Not Offered Every Year
+      <br />
+      <br />
+      <Link to="../">Go Back</Link>
+    </>
+  );
+}
