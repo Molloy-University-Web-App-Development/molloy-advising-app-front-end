004624267dcd0047eb5f4f3bfaa53bdde2b86c59
tpersaud2 <113070356+tpersaud2@users.noreply.github.com>
2022-11-09 21:34:00 -0500

    Add files via upload
    fall 2022 and spring 2023 (not done) academic calendar with navigation bar header and links

---
+++ src/pages/Spring2022.js
@@ -0,0 +1,111 @@
+export default function Spring2022() {
+  return (
+    <>
+      <h1>Spring 2023</h1>
+      <aside id="note">
+        <h2>Note:</h2>
+        <ul>
+          <li>
+            Classes for Spring Long session run the full term beginning in the
+            first week and ending in the last week of Spring term.
+          </li>
+          <li>
+            Subterms: Two <q>Seven Week sessions</q> and four{" "}
+            <q>Three Week sessions</q> are available. Calendar dates and
+            deadlines are listed under Spring 2023 subterms at the end of this
+            section.
+          </li>
+        </ul>
+      </aside>
+
+      <h3>January 2—Monday</h3>
+      <p>New Year's Day (Observed)—College closed.</p>
+      <h3>January 3—Tuesday</h3>
+      <p>Payment of tuition due for advance registered students for Spring.</p>
+      <h3>January 13—Friday</h3>
+      <p>
+        Award date of January degrees and certificates. No ceremony. (All grades
+        for incomplete or missing coursework must be submitted by this date in
+        order for degrees to be awarded with this date.)
+      </p>
+      <h3>January 16—Monday (12:00 noon)</h3>
+      <p>
+        Winter Intersession grades due to the Office of the Registrar by 12:00
+        noon.
+      </p>
+      <h3>January 16—Monday</h3>
+      <p>Martin Luther King, Jr. Day (Observed)—College closed.</p>
+      <h3>January 17—Tuesday</h3>
+      <p>Classes begin for Spring.</p>
+      <h3>January 23—Monday</h3>
+      <p>
+        Last date to Add/Drop a course. Online Registration Open —Office of the
+        Registrar. Full payment of tuition and fees required. (Students with
+        classes scheduled to meet once-a-week for the first time on January 23
+        will have until January 24 to correct their schedules if needed.)
+      </p>
+      <h3>January 24—Tuesday</h3>
+      <p>
+        Withdrawal Period Begins: A grade of "WA" will be assigned from January
+        24, until March 8 or the midpoint of subterms. From March 9 to May, 5 a
+        "WA" or "WF", grade may be assigned. ("WA" grades are not computed in
+        GPA. "WF" Grades are computed as failure in "GPA".) Faculty enter
+        withdrawal grades online for their courses with Last Date of Attendance,
+        if requested or if a student stops attending a class.{" "}
+      </p>
+      <h3>January 24—Tuesday</h3>
+      <p>
+        Registration for auditors, PRIME (age 62 and over) and alumni auditors
+        through the Office of the Registrar. MILL members make arrangements
+        through the Director of Special Programs.
+      </p>
+      <h3>January 25—Wednesday</h3>
+      <p>Incompletes for Fall 2022 and Winter Intersession 2023 become "F."</p>
+      <h3>February 1—Wednesday</h3>
+      <p>
+        Deadline for submitting online health and accident insurance waivers for
+        new full-time undergraduate students.
+      </p>
+      <h3>February 1—Wednesday</h3>
+      <p>Last day to declare a course Pass/Fail.</p>
+      <h3>February 20—Monday</h3>
+      <p>Presidents’ Day—No classes.</p>
+      <h3>February 22—Wednesday</h3>
+      <p>Monday classes held on this day.</p>
+      <h3>March 1—Wednesday</h3>
+      <p>Deadline for filing application for graduation for August 2023.</p>
+      <h3>March 6 - April 12—Monday - Wednesday</h3>
+      <p>
+        Early registration and advisement begins for continuing matriculated
+        students for Summer and Fall. Newly accepted Graduate and new Doctoral
+        students may be registered by their Schools. Registration times are by
+        assigned times for Web Groups. Details will be posted in the Lion's Den
+        Portal.
+      </p>
+      <h3>March 8—Wednesday</h3>
+      <p>Mid-semester appraisals due.</p>
+      <h3>March 9—Thursday</h3>
+      <p>
+        Withdrawal Period Continues: A grade of "WA" will be assigned from
+        January 24 until March 8 or the midpoint of subterms. From March 9 to
+        May, 5 a "WA" or "WF", grade may be assigned. ("WA" grades are not
+        computed in GPA. "WF" Grades are computed as failure in "GPA".) Faculty
+        enter withdrawal grades online for their courses with Last Date of
+        Attendance, if requested or if a student stops attending a class.{" "}
+      </p>
+      <h3>March 13 - 19—Monday - Sunday</h3>
+      <p>Spring Recess—No classes (College offices will remain open.)</p>
+      <h3>April 6- 10—Thursday - Monday</h3>
+      <p>Easter Recess—College closed.</p>
+      <h3>April 12—Wednesday</h3>
+      <p>
+        Early registration for continuing matriculated students ends.
+        Registration will continue as On-going Registration and be open to newly
+        accepted undergraduate students. All 'reserved" courses for Summer or
+        Fall must be approved by the advisor by April 21. Dropped students may
+        register for open seats if available.
+      </p>
+      <h3>April 13—Thursday</h3>
+    </>
+  );
+}
